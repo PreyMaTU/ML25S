f61d8541363c50659a400d6c8c265a4f0cdd665c
Philipp Vanek <e12022484@student.tuwien.ac.at>
2025-05-19 19:44:12 +0200

    UE2: Add BCE loss

--- uebung_2/scratch_net/loss.py
+++ uebung_2/scratch_net/loss.py
@@ -19,6 +19,24 @@ class MSE(LossFunction):
     return 2.0 / output_count * (y_pred - y_true)
 
 
+# TODO: Check correctness of calculation for BCE and CCE
+
+class BinaryCrossEntropy(LossFunction):
+    def forward(self, y_pred, y_true):
+        # Clip to prevent log(0)
+        eps = 1e-15
+        y_pred = np.clip(y_pred, eps, 1 - eps)
+        
+        loss = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+        return loss
+
+    def backward(self, y_pred, y_true):
+        eps = 1e-15
+        y_pred = np.clip(y_pred, eps, 1 - eps)
+
+        # Derivative of BCE w.r.t. y_pred
+        return -(y_true / y_pred - (1 - y_true) / (1 - y_pred)) / y_true.shape[0]
+
 # CCE
 class CategoricalCrossEntropy(LossFunction):
   def forward(self, y_true, y_pred):
