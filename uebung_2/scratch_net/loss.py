65bf4ad7198d2a0e095d6988f14d721aa01cc8cb
Christoph Neubauer <100910774+ChrisNeuba@users.noreply.github.com>
2025-05-20 11:16:59 +0200

    UE2: Adapt BinaryCrossEntropy for grid search

--- uebung_2/scratch_net/loss.py
+++ uebung_2/scratch_net/loss.py
@@ -21,20 +21,19 @@ class MSE(LossFunction):
 # TODO: Check correctness of calculation for BCE and CCE
 
 class BinaryCrossEntropy(LossFunction):
-    def forward(self, y_pred, y_true):
-        # Clip to prevent log(0)
-        eps = 1e-15
-        y_pred = np.clip(y_pred, eps, 1 - eps)
-        
-        loss = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
-        return loss
+  def forward(self, y_true, y_pred):
+    # Clip to avoid log(0) errors
+    eps = 1e-15
+    y_pred = np.clip(y_pred, eps, 1 - eps)
+    
+    return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
 
-    def backward(self, y_pred, y_true):
-        eps = 1e-15
-        y_pred = np.clip(y_pred, eps, 1 - eps)
+  def backward(self, y_true, y_pred):
+    eps = 1e-15
+    y_pred = np.clip(y_pred, eps, 1 - eps)
 
-        # Derivative of BCE w.r.t. y_pred
-        return -(y_true / y_pred - (1 - y_true) / (1 - y_pred)) / y_true.shape[0]
+
+    return -(y_true / y_pred - (1 - y_true) / (1 - y_pred)) / y_pred.shape[0]
 
 # CCE
 class CategoricalCrossEntropy(LossFunction):
